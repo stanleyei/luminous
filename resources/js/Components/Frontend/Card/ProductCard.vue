e7fffc780e8f27c0500ac7934c9d751ad613922d
Stanleyei <81503453+stanleyei@users.noreply.github.com>
2023-12-16 10:45:48 +0800

    feat: 將產品卡片設置為組件
    將產品卡片設置為組件

---
+++ resources/js/Components/Frontend/Card/ProductCard.vue
@@ -0,0 +1,17 @@
+<!-- 前台產品卡片組件 -->
+
+<script setup>
+defineProps({
+  product: Object,
+});
+</script>
+
+<template>
+  <Link :href="route('product.detail', { id: product.id })" :title="`前往${product.name}(跳轉頁面)`">
+    <figure class="flex flex-col gap-y-1 pb-2 bg-[#CCCAB1] text-[#333111] shadow-md">
+      <img :src="product.cover_photo_path" :alt="product.name" class="object-cover w-full h-[210px]">
+      <figcaption class="text-center">{{ product.name }}</figcaption>
+      <b class="block text-center">NT${{ product.price.toLocaleString() }}</b>
+    </figure>
+  </Link>
+</template>
